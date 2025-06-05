810cf611d16608ea2aa12dd7677c5dbb530bd669
italop1 <oliveira.italop@gmail.com>
2025-06-04 23:14:52 -0300

    Desafio_Javascript_Calculadora

--- script-02.js
+++ script-02.js
@@ -4,3 +4,27 @@ function soma(){
   document.getElementById('demo').innerHTML = a + b;
   //document.getElementById('demo').innerHTML = parseInt(a) + parseInt(b);
 }
+
+function subtrair() {
+    var a = document.getElementById('valor01').value;
+    var b = document.getElementById('valor02').value;
+    document.getElementById('demo').innerHTML = parseInt(a) - parseInt(b);
+}
+
+function multiplicar() {
+    var a = document.getElementById('valor01').value;
+    var b = document.getElementById('valor02').value;
+    document.getElementById('demo').innerHTML = parseInt(a) * parseInt(b);
+}
+
+function dividir() {
+    var a = document.getElementById('valor01').value;
+    var b = document.getElementById('valor02').value;
+    document.getElementById('demo').innerHTML = parseInt(a) / parseInt(b);
+}
+
+function potenciacao() {
+    var a = document.getElementById('valor01').value;
+    var b = document.getElementById('valor02').value;
+    document.getElementById('demo').innerHTML = parseInt(a) ** parseInt(b);
+}
